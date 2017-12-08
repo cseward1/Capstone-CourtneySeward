6367fe2d5933f7c0745052c570605058fae808f7
Courtney Seward <courtneyseward93@gmail.com>
2017-12-08 14:28:18 -0600

    figured out issues with the form

--- app/auth/controllers/AuthCtrl.js
+++ app/auth/controllers/AuthCtrl.js
@@ -1,31 +1,31 @@
-//  Controller Page for the Registration or Log In:
+//  Controller Page for Registration & Log In:
 
 angular.module("CapstoneApp")
-.controller("AuthCtrl", function($scope, $location, AuthFactory) {
-    $scope.auth = {}
+    .controller("AuthCtrl", function ($scope, $location, AuthFactory) {
+        $scope.auth = {}
 
-// log out:
-    $scope.logoutUser = function () {
-        AuthFactory.logout()
-        // $location.url('')
-        $location.url('/auth')
-        console.log("You are now logged out, Goodbye!")
-    }
-// log in:
-    $scope.logMeIn = function () {
-        AuthFactory.authenticate($scope.auth).then(function (didLogin) {
-            $scope.login = {}
-            $location.url("/HomePageInformation/homePage")
-            console.log("You are now logged in, Welcome!")
-        })
-    }
-// register:
-    $scope.registerUser = function(registerNewUser) {
-      AuthFactory.registerWithEmail(registerNewUser).then(function (didRegister) {
-        // $scope.logMeIn(registerNewUser)
-        $location.url("/register")
-        console.log("You are now registered in, Welcome!")
-      })
-    }
-    
-})
+        // function to log out:
+        $scope.logoutUser = function () {
+            AuthFactory.logout()
+            // $location.url('')
+            $location.url('/auth')
+            console.log("You are now logged out, Goodbye!")
+        }
+        // function to log in:
+        $scope.logMeIn = function () {
+            AuthFactory.authenticate($scope.auth).then(function (didLogin) {
+                $scope.login = {}
+                $location.url("/HomePageInformation/homePage")
+                console.log("You are now logged in, Welcome!")
+            })
+        }
+        // function to register:
+        $scope.registerUser = function (registerNewUser) {
+            AuthFactory.registerWithEmail(registerNewUser).then(function (didRegister) {
+                // $scope.logMeIn(registerNewUser)
+                $location.url("/register")
+                console.log("You are now registered in, Welcome!")
+            })
+        }
+
+    })
