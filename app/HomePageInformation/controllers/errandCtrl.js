e21f86564cc8a03ef3a650fbf862e4f345508524
Courtney Seward <courtneyseward93@gmail.com>
2017-12-12 16:36:03 -0600

    got my errand list to display

--- app/HomePageInformation/controllers/errandCtrl.js
+++ app/HomePageInformation/controllers/errandCtrl.js
@@ -1,52 +1,39 @@
 // Errand Control Page that displays the Errand List unto the DOM and 
-
 angular.module("CapstoneApp")
     .controller("errandCtrl", function ($scope, AuthFactory, ErrandFactory, $timeout, $location) {
         $scope.products = ["Milk", "Bread", "Cheese"];
-        // this button or ng-click directive  will run the addItem function when the button is clicked
-        // $scope.addItem = function () {
-        //     // $scope.products.push($scope.addMe);
-        //     $scope.errortext = "";
-        //     if (!$scope.addMe) { return; }
-        //     if ($scope.products.indexOf($scope.addMe) == -1) {
-        //         $scope.products.push($scope.addMe);
-        //     } else {
-        //         $scope.errortext = "The item is already in your shopping list.";
-        //     }
-        // }
-        // Call the current
+
+        $scope.saveListItem = function () {
+            $scope.errortext = "";
+            if (!$scope.addMe) {return;}
+            if ($scope.products.indexOf($scope.addMe) == -1) {
+                $scope.products.push($scope.addMe);
+            } else {
+                $scope.errortext = "The item is already in your shopping list.";
+            }
+        } 
+        $scope.removeItem = function (x) {
+            $scope.errortext = "";
+            $scope.errands.splice(x, 1);
+        } 
+
+        // trying to remove list items:
+        $scope.removeItem = function (x) {
+            $scope.errands.splice(x, 1);
+        }
+
+        // call the date 
         $scope.today = new Date();
 
-        // We also want to be able to remove items from the shopping list.
-        // made a function named removeItem, which takes the index of the item you want to remove, as a parameter.
-        $scope.removeItem = function (x) {
-            // checking value and throwing error if already inputted
-            $scope.errortext = "";
-            $scope.products.splice(x, 1);
 
-        }
-        
+        // keep the list updated and displaying
         ErrandFactory.list(AuthFactory.getUser().uid)
-        .then(errandList => {
-            $timeout()
-            $scope.errandList = errandList
-            console.log("ErrandsList", $scope.errandList)
-        })
+            .then(errandList => {
+                $timeout()
+                $scope.errandList = errandList
+                console.log("ErrandsList", $scope.errandList)
+            })
 
-        // NEXT STEP: 
-        // grab the information from your Grocery List and POST to Firebase attached to that parent Id
-
-        // $scope.$on('$viewContentLoaded',function() {
-        //     if (!ErrandFactory.cache) {
-        //         console.info("No cached data")
-        //         ErrandFactory.list(AuthFactory.getUser().uid).then(data => {
-        //             $scope.errandList = data
-        //         })
-        //     } else {
-        //         console.info("Using cached data")
-        //         $scope.apply($scope.errandList = ErrandFactory.cache)
-        //     }
-        // })
 
         $scope.saveListItem = function () {
             // console.log("user!", firebase.auth().currentUser.id)
@@ -61,7 +48,7 @@ angular.module("CapstoneApp")
 
             ErrandFactory.add(errand).then(() => {
                 // $scope.Date.now() = ""
-                $scope.storeName = ""
+                // $scope.storeName = ""
                 $scope.itemName = ""
 
             })
