ca5fbe9d7f13fbbb1336d3f7c4fa20df2f79d142
Courtney Seward <courtneyseward93@gmail.com>
2017-12-13 17:43:00 -0600

    got the api to display the image,ingrients to display on the health page

--- app/HomePageInformation/controllers/healthCtrl.js
+++ app/HomePageInformation/controllers/healthCtrl.js
@@ -1,31 +1,30 @@
 
 // When the form is filled and saved the "click" event will route the user to the home page:
 angular.module("CapstoneApp")
-    .controller("healthCtrl", function (healthFactory, AllergyFactory, $scope, $location) {
+.controller("healthCtrl", function (healthFactory, AllergyFactory, $scope, $location) {
 
 
-        console.log("the health page is dispalying")
+    console.log("the health page is dispalying")
 
-        // Meta Data from the Yummly App that I hard coded in - didn't understand how to do a "get" request for the meta data
-        $scope.allergies = AllergyFactory
-        // function:
-        // 1. Get the values of the search field:
-        $scope.searchRecipe = function (allergy) {
-            console.log(allergy)
-            healthFactory.getRecipes($scope.searchString,allergy.allergyCode).then((Recipes) => {
-                console.log(Recipes)
-                $scope.$apply(function(){
-                    $scope.Recipes = Recipes 
-                })
-                
-                $scope.recipeName = ""
-                $scope.allergySearch = ""
+    // Meta Data from the Yummly App that I hard coded in - didn't understand how to do a "get" request for the meta data
+    $scope.allergies = AllergyFactory
+    // function:
+    // 1. Get the values of the search field:
+    $scope.searchRecipe = function (allergy) {
+        console.log(allergy)
+        healthFactory.getRecipes($scope.searchString,allergy.allergyCode).then((Recipes) => {
+            console.log(Recipes)
+            $scope.$apply(function(){
+                $scope.Recipes = Recipes 
             })
-        }
+            
+            $scope.recipeName = ""
+            $scope.allergySearch = ""
+        })
+    }
 
-        // 2. String replace- go through the string and add a plus the api takes a 
-        // 3.value of the allergy and the string with plus and send to the api
+    // 2. String replace- go through the string and add a plus the api takes a 
+    // 3.value of the allergy and the string with plus and send to the api
 
 
-    })
-
+})
