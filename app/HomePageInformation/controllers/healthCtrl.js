99361d9c305c389a0ee3ab2d2277c0e8a17accfe
Courtney Seward <courtneyseward93@gmail.com>
2017-12-13 16:59:32 -0600

    got the food to display and the allergy filter working

--- app/HomePageInformation/controllers/healthCtrl.js
+++ app/HomePageInformation/controllers/healthCtrl.js
@@ -1,3 +1,4 @@
+
 // When the form is filled and saved the "click" event will route the user to the home page:
 angular.module("CapstoneApp")
     .controller("healthCtrl", function (healthFactory, AllergyFactory, $scope, $location) {
@@ -6,23 +7,25 @@ angular.module("CapstoneApp")
         console.log("the health page is dispalying")
 
         // Meta Data from the Yummly App that I hard coded in - didn't understand how to do a "get" request for the meta data
-         $scope.allergies = AllergyFactory
+        $scope.allergies = AllergyFactory
         // function:
-        // 1. get the values of the search field and the drop down - ng-model call that variable
-        $scope.searchRecipe = function (){
-            healthFactory.getRecipes($scope.searchString).then(() => {
+        // 1. Get the values of the search field:
+        $scope.searchRecipe = function (allergy) {
+            console.log(allergy)
+            healthFactory.getRecipes($scope.searchString,allergy.allergyCode).then((Recipes) => {
+                console.log(Recipes)
+                $scope.$apply(function(){
+                    $scope.Recipes = Recipes 
+                })
+                
                 $scope.recipeName = ""
-                $scope.allergySearch= ""
+                $scope.allergySearch = ""
             })
         }
 
-           //  Use the factory to POST to Firebase:
-      
-    
+        // 2. String replace- go through the string and add a plus the api takes a 
+        // 3.value of the allergy and the string with plus and send to the api
 
-// 2.string replace- go through the string and add a plus the api takes a 
- // 3.value of the allergy and the string with plus and send to the api
-       
 
     })
 
--- app/HomePageInformation/factories/healthFactory.js
+++ app/HomePageInformation/factories/healthFactory.js
@@ -10,22 +10,25 @@ angular
 
 
             // Performing GET requests to search for recipes
-            // when do I put in the parameters?
-
             // authorize the user with Firebase (that is why you are calling the token ID):
             "getRecipes": {
-                value: function (searchString) {
+                value: function (searchString, allergy) {
                     return firebase.auth().currentUser.getToken(true)
                         .then(idToken => {
                             return $http({
                                 method: "GET",
-                                url: `http://api.yummly.com/v1/api/recipes?_app_id=7633e14a&_app_key=802aa25ab2fcd5e3374f66b206bf30b8&q="${searchString}"`
+                                url: `http://api.yummly.com/v1/api/recipes?_app_id=7633e14a&_app_key=802aa25ab2fcd5e3374f66b206bf30b8&q="${searchString}"&requirePictures=true&allowedAllergy[]=${allergy}`
                             })
                         }).then(response => {
                             return response.data.matches
                         })
                 }
             },
+
+            // Insert Pictures on the Page:
+            // &requirePictures=true
+            // Take in the Allergy Filter:
+            // allowedAllergy[]
 
             // Display the search list unto the Health Page:
             // "single": {
